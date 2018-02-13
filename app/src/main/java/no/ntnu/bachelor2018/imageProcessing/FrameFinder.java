308d6278c1484920965dcc492b9397cf9562e431
christian.hadem@hig.no <Christian.Hadem@stud.ntnu.no>
2018-02-13 09:10:09 +0100

    Added new classes for image processing

---
+++ app/src/main/java/no/ntnu/bachelor2018/imageProcessing/FrameFinder.java
@@ -0,0 +1,32 @@
+package no.ntnu.bachelor2018.imageProcessing;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bruker on 13.02.2018.
+ */
+
+public class FrameFinder {
+
+    FrameFinder(){
+
+    }
+
+    /**
+     * Returns a list of the coordinates at the edge
+     *
+     * @param mat
+     * @return
+     */
+    public List<MatOfPoint> findFrame(Mat mat){
+        List<MatOfPoint> contours = new ArrayList<>();
+        Imgproc.findContours(mat, contours, null, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        return contours;
+    }
+
+}
---
+++ app/src/main/java/no/ntnu/bachelor2018/imageProcessing/MarkerDetection.java
@@ -0,0 +1,13 @@
+package no.ntnu.bachelor2018.imageProcessing;
+
+/**
+ * Created by bruker on 13.02.2018.
+ */
+
+public class MarkerDetection {
+
+    MarkerDetection(){
+        
+    }
+
+}
