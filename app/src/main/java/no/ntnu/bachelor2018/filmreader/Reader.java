f1ca5fecce679213be94abe110f0d45663a1198b
470175@hig.no <heggholmen3@gmail.com>
2018-02-19 18:24:24 +0100

    Researched marker detection finding and improoved frame finder.

--- app/src/main/java/no/ntnu/bachelor2018/filmreader/Reader.java
+++ app/src/main/java/no/ntnu/bachelor2018/filmreader/Reader.java
@@ -1,11 +1,17 @@
 package no.ntnu.bachelor2018.filmreader;
 
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 
+import java.util.Vector;
+
 import no.ntnu.bachelor2018.imageProcessing.FrameFinder;
+import no.ntnu.bachelor2018.imageProcessing.MarkerDetection;
 
 /**
  * Created by hcon on 13.02.18.
@@ -16,16 +22,19 @@ public class Reader {
     private final String TAG = this.getClass().getSimpleName();
 
     private FrameFinder finder;
+    private MarkerDetection markDetect;
     private int width, height;
-    private Mat grayImg;
+    private Mat grayImg, threshImg;
+    private Vector<Point> corners;
 
     public Reader(int width, int height){
         //TODO: Håkon add camera config parameter constructor
         finder = new FrameFinder(width,height);
+        markDetect = new MarkerDetection(width,height);
         this.width = width;
         this.height = height;
         grayImg = new Mat(height, width, CvType.CV_8UC1);
-
+        threshImg = new Mat();
     }
 
      /**
@@ -36,7 +45,14 @@ public class Reader {
     public Mat processFrame(Mat inputImage){
         // Saves a grayscale image to the variable 'grayImg'
         Imgproc.cvtColor(inputImage, grayImg, Imgproc.COLOR_BGR2GRAY);
-
+        //Apply adaptive threshold
+        Imgproc.adaptiveThreshold(grayImg,threshImg,255,Imgproc.ADAPTIVE_THRESH_MEAN_C,Imgproc.THRESH_BINARY_INV,301,8);
+        corners = finder.cornerFinder(threshImg);
+        for (int i = 0; i<corners.size(); i++){
+            Imgproc.line(inputImage,corners.get(i),corners.get((i + 1) %corners.size()),new Scalar(255,255,255));
+            Imgproc.putText(inputImage,"" +i,corners.get(i), Core.FONT_HERSHEY_PLAIN,10,new Scalar(0,0,0),10);
+        }
+        //markDetect.findMarkers(threshImg);
         return inputImage;
     }
 }
--- app/src/main/java/no/ntnu/bachelor2018/imageProcessing/MarkerDetection.java
+++ app/src/main/java/no/ntnu/bachelor2018/imageProcessing/MarkerDetection.java
@@ -1,13 +1,72 @@
 package no.ntnu.bachelor2018.imageProcessing;
 
+import android.util.Log;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.core.TermCriteria;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 /**
  * Created by bruker on 13.02.2018.
  */
 
 public class MarkerDetection {
 
-    MarkerDetection(){
-        
+    private final String TAG = this.getClass().getSimpleName();
+
+    private int width, height;
+    private Mat contourImage, emptyMask;
+    private MatOfPoint corners;
+    private MatOfPoint2f corners2f;
+
+    private final Size windowSize = new Size(21,21);
+    private final Size zeroZone = new Size(1,1);
+
+    private final TermCriteria criteria = new TermCriteria(TermCriteria.EPS | TermCriteria.MAX_ITER, 40, 0.001 );
+
+    public MarkerDetection(int width, int height){
+        this.width = width;
+        this.height = height;
+        contourImage = new Mat(height, width, CvType.CV_8UC1);
+        corners = new MatOfPoint();
+        corners2f = new MatOfPoint2f();
+        emptyMask = new Mat();
+
+    }
+
+    /**
+     * Finds corner markers.
+     * @param image
+     * @return
+     */
+    public Mat findMarkers(Mat image){
+        //Apply adaptive threshold
+        Point[] pointArr;
+        //Imgproc.adaptiveThreshold(image,contourImage,255,Imgproc.ADAPTIVE_THRESH_MEAN_C,Imgproc.THRESH_BINARY,301,8);
+
+        //Imgproc.cornerHarris(contourImage,corners2f,50,31,0.04);
+        Imgproc.goodFeaturesToTrack(image,corners,20,0.5,10);
+        corners.convertTo(corners2f,CvType.CV_32FC(2));
+
+        Imgproc.cornerSubPix(image,corners2f,windowSize,zeroZone,criteria);
+        pointArr = corners2f.toArray();
+        for(Point pt: pointArr){
+            Imgproc.circle(image,pt,10,new Scalar(255,255,255));
+        }
+        return image;
+
     }
 
 }
